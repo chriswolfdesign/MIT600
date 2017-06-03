afae956af616c08a2865afa51fbc229dcd74a080
Chris Wolf <chriswolf@chriss-mbp.home>
2017-06-03 08:56:00 -0400

    Attempted solution: pset6 Problem 3

--- psets/pset6/ps6.py
+++ psets/pset6/ps6.py
@@ -15,6 +15,8 @@ SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
+points_dict = {}
+
 # -----------------------------------
 # Helper code
 # (you don't need to understand this helper code)
@@ -81,6 +83,20 @@ def get_word_score(word, n):
     if len(word) == n:
         score += 50.0
     return score
+
+def get_words_to_points(word_list):
+    """
+    Returns a dictionary containing our entire word list
+    translated into a dictionary, pairing each word with the points the word
+    is worth.
+
+    word_list: The array of words created earlier in the program.
+    returns: string -> int
+    """
+    return_dict = {}
+    for word in word_list:
+        return_dict[word] = get_word_score(word)
+    return return_dict
 
 #
 # Make sure you understand how this function works and what it does!
